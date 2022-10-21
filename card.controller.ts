7afc439ce0466988d4de3678a9f7411b43cc7cfa
dennishoooo <105833202+dennishoooo@users.noreply.github.com>
2022-10-21 11:51:10 +0800

    remove this

--- card.controller.ts
+++ card.controller.ts
@@ -2,9 +2,7 @@ import { Request, Response } from "express";
 import { CardService } from "./card.service";
 
 export class CardController {
-  constructor(private cardService: CardService) {
-    this.cardService = cardService;
-  }
+  constructor(private cardService: CardService) {}
   getProcessID(req: Request, res: Response) {
     let uuid = this.cardService.getProcessID();
     res.json({ uuid });
--- card.service.ts
+++ card.service.ts
@@ -2,10 +2,7 @@ import { getFetchApi } from "./helpers";
 import { Url } from "./types";
 
 export class CardService {
-  constructor(private url: Url, private accessToken: string) {
-    this.url = url;
-    this.accessToken = accessToken;
-  }
+  constructor(private url: Url, private accessToken: string) {}
   async getProcessID() {
     let result = await getFetchApi(this.url.processIDUrl, this.accessToken);
     let processID = result.GetReleasedPIDResult;
