a5be6cde7fc4aa2f4af81fbfee5ab709be6a172e
dennishoooo <105833202+dennishoooo@users.noreply.github.com>
2022-10-21 11:46:43 +0800

    add types for express

--- card.controller.ts
+++ card.controller.ts
@@ -1,10 +1,11 @@
+import { Request, Response } from "express";
 import { CardService } from "./card.service";
 
 export class CardController {
   constructor(private cardService: CardService) {
     this.cardService = cardService;
   }
-  getProcessID(req, res) {
+  getProcessID(req: Request, res: Response) {
     let uuid = this.cardService.getProcessID();
     res.json({ uuid });
   }
